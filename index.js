5a0d6f7b4dde2de0ed86c15c75f093d53b0a6886
lebrancconvas <lebrancconvas@gmail.com>
2021-12-17 02:58:34 +0700

    🔨 Connect Mongoose.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 8001;
+
+mongoose.connect('mongodb://localhost/ourdata');
+mongoose.Promise = global.Promise;
 
 app.get('/', (req, res) => {
     res.send(`Test.`);
@@ -9,6 +13,8 @@ app.get('/', (req, res) => {
 app.get('/admin', (req, res) => {
     res.send(`API Testing.`);
 })
+
+
 
 app.listen(port, () => {
     console.log(`Server Listening at PORT ${port}`);
