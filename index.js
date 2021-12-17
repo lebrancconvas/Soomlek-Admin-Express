0417d42f91c76345d8037ac82a42ec7f162caeec
lebrancconvas <lebrancconvas@gmail.com>
2021-12-17 23:27:43 +0700

    🔨 Add Path and Edit Server.

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8000;
 
-mongoose.connect('mongodb://localhost/ourdata');
-mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/ourdata');
+// mongoose.Promise = global.Promise;
 
 // app.get('/', (req, res) => {
-//     res.send(`Test.`);
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // })
 
 // app.get('/admin', (req, res) => {
 //     res.send(`API Testing.`);
 // })
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', require('./routes/api'));
+// app.use(express.json());
 
-app.use((err, req, res, next) => {
-    res.status(422).send({ error: err.message });
-})
+// app.use('/api', require('./routes/api'));
+
+// app.use((err, req, res, next) => {
+//     res.status(422).send({ error: err.message });
+// })
 
 
 app.listen(port, () => {
