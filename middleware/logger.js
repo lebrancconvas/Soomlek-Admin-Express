501a0ea2542ecdf07d7a77bb50c80443250bc754
lebrancconvas <lebrancconvas@gmail.com>
2021-12-18 19:05:16 +0700

    🔨 Add Moment in the Middleware.

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,7 @@
+const moment = require('moment');
+
 const logger = (req, res, next) => {
-    console.log(`Test Middleware.`);
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
     next();
 }
 
