0afa2a975b14f9fcad58739ebc00e4ab15604459
lebrancconvas <lebrancconvas@gmail.com>
2021-12-18 21:50:48 +0700

    🗃️ Add Data.

--- data/Questions.js
+++ data/Questions.js
@@ -3,6 +3,11 @@ const questions = [{
     "question": "5x = 10, x = ?",
     "options": ["1", "2", "3", "4"],
     "answer": "2"
+}, {
+    "id": 2,
+    "question": "x^2 + 2x + 1 = 0; x = ?",
+    "options": ["-2", "-1", "0", "1"],
+    "answer": "-1"
 }];
 
 module.exports = questions;
