22d4ec1dd6a626f848896f03e245d83aa4bba403
lebrancconvas <lebrancconvas@gmail.com>
2021-12-20 05:12:34 +0700

    🔨 Update View Engine from "handlebars" to "EJS".

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { engine } = require('express-handlebars');
 app.use(logger);
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', {
